12f4b729bb1d6866b6fdc0133bfe92a036bdd6fe
TalhaShaikh150 <tssmart233@gmail.com>
2025-11-17 11:58:18 +0500

    Fixed Delete Button Visibility Issue

--- src/Components/Todo.jsx
+++ src/Components/Todo.jsx
@@ -86,7 +86,7 @@ export const Todo = () => {
                   </div>
                   <button
                     onClick={() => handleRemoveTodo(todo.id)}
-                    className="p-2 text-gray-400 hover:text-red-500 hover:bg-red-50 rounded-lg transition-all duration-200 opacity-0 group-hover:opacity-100"
+                    className="p-2 text-gray-900 hover:text-red-500 cursor-pointer hover:bg-red-50 rounded-lg transition-all duration-200 group-hover:opacity-100 hover:scale-110"
                   >
                     <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                       <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
